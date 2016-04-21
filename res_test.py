57e4ae4a80d5e3d375f5c1e851b1689b1b35b078
John <johnlewis0945@gmail.com>
2016-04-21 17:16:01 +0200

    changed net.cmd to cmd

--- res_test.py
+++ res_test.py
@@ -30,7 +30,7 @@ def testing(net, filename, host1, host2, switch1, switch2):
     h1.cmd("ping -i 0.01{} &".format(h2.IP()))
     h2.cmd("tcpdump -XX -n -i {}-eth0 -w {}.pcap  &".format(host2, filename))
     sleep(0.5)
-    net.cmd("link {} {} down ".format(s1, s2))
+    cmd("link {} {} down ".format(s1, s2))
     sleep(1)
 
     h1.cmd("kill %ping")

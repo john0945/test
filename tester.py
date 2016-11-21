b70609f2328ef63975155347cad51d961628ecd6
Ubuntu <ubuntu@ip-172-31-24-227.eu-west-1.compute.internal>
2016-11-21 14:43:12 +0000

    10x run on backup

--- tester.py
+++ tester.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python
-
 import os
 from optparse import OptionParser
 
 from mininet.net import Mininet
 from mininet.topo import Topo
 from mininet.node import RemoteController, UserSwitch, Host
-from mininet.link import TCLink
+
+from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 from mininet.cli import CLI
-import usnet, backup
+import usnet, backup, ag_n_test
+from time import ctime, sleep
 from mininet.node import OVSSwitch
 from functools import partial
 import itertools
@@ -54,10 +54,15 @@ def node_test(net):
     for switch in switches:
         print (switch.name)
 
+
+
         if switch.name[1] == '1' or switch.name[1] == '7':
             print ('skip')
 
         else:
+            filename = switch.name
+            os.system("sudo tshark -n -i eth0 -T text > tester_results/{}.txt  &".format(filename))
+
             for k, v in switch.ports.items():
                 if k.name[0] == 's':
                     switch.cmd("ifconfig {} down &".format(k))
@@ -66,6 +71,8 @@ def node_test(net):
 
             sleep(1)
             net.pingAll()
+            os.system("sudo pkill tshark")
+            append_results(filename)
 
             for k, v in switch.ports.items():
                 if k.name[0] == 's':
@@ -86,15 +93,28 @@ def link_test(net):
 
         print(intf_name + "       " + intf_name2)
 
+
         if intf_name[0] =='s' and intf_name2[0] == 's':
+            filename = intf_name + '-' + intf_name2
+
+            os.system("sudo tshark -n -i eth0 -T text > tester_results/{}.txt  &".format(filename))
+            sleep(5)
             node1.cmd("ifconfig {} down &".format(intf_name))
             node2.cmd("ifconfig {} down &".format(intf_name2))
 
-            sleep(1)
-            net.pingAll()
+            sleep(5)
+
+            # net.pingAll()
+            os.system("sudo pkill tshark")
+            append_results(filename)
 
             node1.cmd("ifconfig {} up &".format(intf_name))
             node2.cmd("ifconfig {} up &".format(intf_name2))
+            sleep(2)
+
+
+
+
 
 
 def agg_test(net):
@@ -110,8 +130,9 @@ def agg_test(net):
         switch2 = pair[1]
 
         print(switch1.name + "   " + switch2.name)
+        filename = switch1.name + '-'+ switch2.name
 
-        os.system("sudo tshark -n -i eth0 -T text > tester_results/{}-{}.txt".format(switch1.name, switch2.name))
+        os.system("sudo tshark -n -i eth0 -T text > tester_results/{}.txt  &".format(filename))
 
         for intf in switch1.intfs:
             if intf.link.intf2.node == switch2:
@@ -127,7 +148,7 @@ def agg_test(net):
         net.pingAll()
 
         os.system("sudo pkill tshark")
-
+        append_results(filename)
         for intf in switch1.intfs:
             if intf.link.intf2.node == switch2:
                 intf_name = intf.name
@@ -135,6 +156,33 @@ def agg_test(net):
 
                 switch1.cmd("ifconfig {} up &".format(intf_name))
                 switch2.cmd("ifconfig {} up &".format(intf_name2))
+
+def append_results(filename):
+    log = open("log.txt", 'a')
+
+    print("./tester_results/{}.txt".format(filename))
+    with open("./tester_results/{}.txt".format(filename), 'r') as file:
+        start = -1
+        first = -1
+        last = -1
+        mods = 0
+        for line in file:
+            l= line.split(' ')
+            # print(l)
+            if 'OFPT_PORT_STATUS\n' in l and start == -1:
+                start = float(l[1])
+                print(start)
+
+            if 'OFPT_FLOW_MOD\n' in l:
+                print("found flow mod")
+                mods += 1
+                if first == -1:
+                    first = float(l[1]) - start
+                last = float(l[1]) - start
+
+        log.write("{},{},{},{},{}\n".format(filename, start, first, last, mods))
+
+    log.close()
 
 
 def config(opts):
@@ -152,6 +200,8 @@ def config(opts):
         topo_func = usnet.USNET()
     if topo == 'backup':
         topo_func = backup.backup()
+    if topo == 'agg':
+        topo_func = ag_n_test.USNET()
 
     switch = partial( OVSSwitch, protocols='OpenFlow13' )
     net = Mininet(topo=topo_func, switch=switch, controller=None)
@@ -170,7 +220,8 @@ def config(opts):
     if test == 'node':
         node_test(net)
     if test == 'link':
-        link_test(net)
+        for i in range(20):
+            link_test(net)
     if test == 'agg':
         agg_test(net)
 
